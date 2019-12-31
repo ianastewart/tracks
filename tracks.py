4c620a830ff226b60546eaf107a3645a77549b8e
Ian <is@ktconsultants.co.uk>
2019-12-31 16:23:37 +0000

    Runs but needs optimisation

--- tracks.py
+++ tracks.py
@@ -17,27 +17,114 @@ class Content(Enum):
 
 
 class Cell:
-    def __init__(self, row, col):
-        self.left = False
-        self.right = False
-        self.top = False
-        self.bottom = False
+    def __init__(self, row, col, cell_size):
+        self.cell_size = cell_size
+        self.x = col * self.cell_size + self.cell_size / 2
+        self.y = row * self.cell_size + self.cell_size / 2
+        self.row = row
+        self.col = col
         self.content = Content.EMPTY
         self.track = None
         self.must_connect = False
-        self.row = row
-        self.col = col
         self.is_start = False
         self.is_end = False
+
+    def __str__(self):
+        return f"Cell {self.row} {self.col} {self.content} {self.track}"
 
     def is_empty(self):
         return self.content == Content.EMPTY
 
     def has_dir(self, dir):
         if self.content != Content.EMPTY:
-            if dir in self.track.name:
-                return True
+            if self.track:
+                if dir in self.track.name:
+                    return True
 
+    def draw_border(self, t):
+        t.pensize(1)
+        t.setpos(self.x, self.y)
+        t.pendown()
+        t.setheading(0)
+        color = "gray"
+        t.color(color)
+        t.forward(self.cell_size - 4)
+        t.left(90)
+        t.forward(self.cell_size - 4)
+        t.left(90)
+        t.forward(self.cell_size - 4)
+        t.left(90)
+        t.forward(self.cell_size - 4)
+        t.penup()
+
+    def draw_track(self, t, erase=False):
+        """ draw the track piece in the cell """
+        s = self.cell_size
+        s2 = s / 2
+        x1 = self.x + s2
+        x2 = self.x + s
+        y1 = self.y + s2
+        y2 = self.y + s
+        t.pensize(4)
+        t.penup()
+        if self.must_connect:
+            t.goto(x1, y1)
+            t.dot(10, "red")
+        if self.track:
+            color = "red"
+            if self.content == Content.TEMP_TRACK:
+                color = "yellow" if erase else "orange"
+            elif self.content == Content.PERMANENT:
+                color = "blue"
+            t.color(color)
+            if self.track == Track.NS:
+                t.setheading(90)
+                t.goto(x1, self.y)
+                t.pendown()
+                t.forward(s)
+            elif self.track == Track.EW:
+                t.setheading(0)
+                t.goto(self.x, y1)
+                t.pendown()
+                t.forward(s)
+            elif self.track == Track.NE:
+                t.goto(x1, y2)
+                t.pendown()
+                t.goto(x2, y1)
+                # t.circle(-r, -90)
+            elif self.track == Track.SE:
+                t.goto(x1, self.y)
+                t.pendown()
+                t.goto(x2, y1)
+                # t.circle(r, -90)
+            elif self.track == Track.NW:
+                t.goto(x1, y2)
+                t.pendown()
+                t.goto(self.x, y1)
+                # t.circle(r, -90)
+            elif self.track == Track.SW:
+                t.goto(x1, self.y)
+                t.pendown()
+                t.goto(self.x, y1)
+                # t.circle(-r, 90)
+            t.penup()
+        # else:
+        #     t.goto(x + 2, y + 2)
+        #     t.setheading(0)
+        #     t.begin_fill()
+        #     t.color('white')
+        #     t.pendown()
+        #     t.forward(x - 4)
+        #     t.right(90)
+        #     t.forward(x - 4)
+        #     t.right(90)
+        #     t.forward(x - 4)
+        #     t.right(90)
+        #     t.forward(x - 4)
+        #     t.right(90)
+        #     t.end_fill()
+        #     t.penup()
+        
 class Layout:
     def __init__(self, size=8):
         self.size = size
@@ -46,7 +133,7 @@ class Layout:
         self.turtle = turtle.Turtle()
         turtle.mode('standard')
 
-        turtle.screensize(1000,1000)
+        turtle.screensize(1000, 1000)
         self.screen_size = 1000
         self.cell_size = self.screen_size/(size + 1)
         turtle.setworldcoordinates(0, 0, self.screen_size, self.screen_size)
@@ -60,19 +147,21 @@ class Layout:
         for row in range(size):
             col_list = []
             for col in range(size):
-                col_list.append(Cell(row, col))
+                col_list.append(Cell(row, col, self.cell_size))
             self.layout.append(col_list)
         # Add borders
-        for i in range(size):
-            self.layout[0][i].bottom = True
-            self.layout[size -1 ][i].top = True
-            self.layout[i][0].left = True
-            self.layout[i][size - 1].right = True
+        # for i in range(size):
+        #     self.layout[0][i].bottom = True
+        #     self.layout[size -1 ][i].top = True
+        #     self.layout[i][0].left = True
+        #     self.layout[i][size - 1].right = True
 
     def draw(self):
         for row in range(self.size):
             for col in range(self.size):
-                self.draw_cell(row, col)
+                cell = self.layout[row][col]
+                cell.draw_border(self.turtle)
+                cell.draw_track(self.turtle)
         turtle.update()
         row = self.size
         font = font=('sans-serif', 18, 'normal')
@@ -103,92 +192,12 @@ class Layout:
         y = row * self.cell_size + self.cell_size / 2
         return x, y
 
-    def draw_cell(self, row, col):
-        cell = self.layout[row][col]
-        x, y = self.coords(row, col)
-        self.turtle.pensize(1)
-        self.turtle.setpos(x, y)
-        self.turtle.pendown()
-        self.turtle.setheading(0)
-        color = "red" if cell.bottom else "gray"
-        self.turtle.color(color)
-        self.turtle.forward(self.cell_size-4 )
-        self.turtle.left(90)
-        color = "red" if cell.right else "gray"
-        self.turtle.color(color)
-        self.turtle.forward(self.cell_size - 4)
-        self.turtle.left(90)
-        color = "red" if cell.top else "gray"
-        self.turtle.color(color)
-        self.turtle.forward(self.cell_size - 4)
-        self.turtle.left(90)
-        color = "red" if cell.left else "gray"
-        self.turtle.color(color)
-        self.turtle.forward(self.cell_size - 4)
-        self.turtle.penup()
-        self.draw_moves(cell, x, y)
-        self.draw_track(cell, x, y)
-        self.turtle.penup()
-        turtle.update()
-
-
     def draw_moves(self, cell, x, y):
         """ debug routine to list all possible moves from a cell """
         self.turtle.goto(x,y)
         self.turtle.write(self.moves(cell))
 
-    def draw_track(self, cell, x, y):
-        """ draw the track piece in the cell """
-        t = self.turtle
-        s = self.cell_size
-        s2 = s / 2
-        x1 = x + s2
-        x2 = x + s
-        y1 = y + s2
-        y2 = y + s
-        t.pensize(4)
-        t.penup()
-        if cell.must_connect:
-            t.goto(x1,y1)
-            t.dot(10, "red")
-        if cell.track:
-            color='gray'
-            if cell.content == Content.TEMP_TRACK:
-                color = ("blue")
-            elif cell.content == Content.PERMANENT:
-                color = ("orange")
-            self.turtle.color(color)
-            if cell.track == Track.NS:
-                t.setheading(90)
-                t.goto(x1, y)
-                t.pendown()
-                t.forward(s)
-            elif cell.track == Track.EW:
-                t.setheading(0)
-                t.goto(x, y1)
-                t.pendown()
-                t.forward(s)
-            elif cell.track == Track.NE:
-                t.goto(x1, y2)
-                t.pendown()
-                t.goto(x2, y1)
-                # t.circle(-r, -90)
-            elif cell.track == Track.SE:
-                t.goto(x1, y)
-                t.pendown()
-                t.goto(x2, y1)
-                # t.circle(r, -90)
-            elif cell.track == Track.NW:
-                t.goto(x1, y2)
-                t.pendown()
-                t.goto(x, y1)
-                # t.circle(r, -90)
-            elif cell.track == Track.SW:
-                t.goto(x1, y)
-                t.pendown()
-                t.goto(x, y1)
-                # t.circle(-r, 90)
-
+    
 
     def set_constraints(self, values):
         """ Takes string of 16 numbers representing top and right side """
@@ -238,25 +247,25 @@ class Layout:
         r2 = cell.row + 1
         c1 = cell.col - 1
         c2 = cell.col + 1
-        if r2 < self.size and cell.is_empty() or cell.has_dir("N"):
+        if r2 < self.size and (cell.is_empty() or cell.has_dir("N")):
             if self.layout[r2][cell.col].content == Content.EMPTY:
                 result.append("N")
-            elif "S" in self.layout[r2][cell.col].track.name:
+            elif self.layout[r2][cell.col].has_dir("S"):
                 result.append("N")
-        if r1 >= 0 and cell.is_empty() or cell.has_dir("S"):
+        if r1 >= 0 and (cell.is_empty() or cell.has_dir("S")):
             if self.layout[r1][cell.col].content == Content.EMPTY:
                 result.append("S")
-            elif "N" in self.layout[r1][cell.col].track.name:
+            elif self.layout[r1][cell.col].has_dir("N"):
                 result.append("S")
-        if c1 >= 0 and cell.is_empty() or cell.has_dir("W"):
+        if c1 >= 0 and (cell.is_empty() or cell.has_dir("W")):
             if self.layout[cell.row][c1].content == Content.EMPTY:
                 result.append("W")
-            elif "E" in self.layout[cell.row][c1].track.name:
+            elif self.layout[cell.row][c1].has_dir("E"):
                 result.append("W")
-        if c2 < self.size and cell.is_empty() or cell.has_dir("E"):
+        if c2 < self.size and (cell.is_empty() or cell.has_dir("E")):
             if self.layout[cell.row][c2].content == Content.EMPTY:
                 result.append("E")
-            elif "W" in self.layout[cell.row][c2].track.name:
+            elif self.layout[cell.row][c2].has_dir("W"):
                 result.append("E")
         if cell.is_start and "W" in result:
             result.remove("W")
@@ -270,62 +279,83 @@ class Layout:
         for row in range(self.size):
             count = 0
             for col in range(self.size):
-                if self.layout[row][col].content != Content.EMPTY:
+                cell = self.layout[row][col]
+                if not cell.is_empty():
                     count += 1
+                if exact:
+                    if cell.must_connect and cell.is_empty():
+                        print("Must connect failure")
+                        return False
             if exact:
                 if count != self.row_constraints[row]:
+                    print(f"Exact Row {row} failure {count} != {self.row_constraints[row]}")
                     return False
             else:
                 if count > self.row_constraints[row]:
+                    print(f"Row {row} failure {count} > {self.row_constraints[row]}")
                     return False
         for col in range(self.size):
             count = 0
             for row in range(self.size):
-                if self.layout[row][col].content != Content.EMPTY:
+                cell = self.layout[row][col]
+                if not cell.is_empty():
                     count += 1
             if exact:
                 if count != self.col_constraints[col]:
+                    print(f"Exact column {col} failure {count} != {self.col_constraints[col]}")
                     return False
             else:
                 if count > self.col_constraints[col]:
+                    print(f"Column {col} failure {count} > {self.col_constraints[col]}")
                     return False
         return True
 
-    def move_to(self, row, col, first_move=False):
-        cell = self.layout[row][col]
-        if cell.content == Content.PERMANENT:
-            if (row, col) == self.start and not first_move:
-                return False
-            elif (row, col) == self.end:
-                if self.check_constraints(exact = True):
-                    print("FINISHED")
-                    return True
-        elif cell.content == Content.TEMP_TRACK:
-            return False
-        if cell.content == Content.EMPTY:
-            cell.content = Content.TEMP_TRACK
-        self.draw_cell(row, col)
+    def move_from(self, cell, dir):
+        """ move from cell in direction dir  """
+        print (cell, dir)
+        self.draw()
+        if dir == "N":
+            from_dir = "S"
+            new_cell = self.layout[cell.row + 1][cell.col]
+        elif dir == "S":
+            from_dir = "N"
+            new_cell = self.layout[cell.row - 1][cell.col]
+        elif dir == "E":
+            from_dir = "W"
+            new_cell = self.layout[cell.row][cell.col+1]
+        elif dir =="W":
+            from_dir = "E"
+            new_cell = self.layout[cell.row][cell.col-1]
+        undo = False
+        if new_cell.content == Content.EMPTY:
+            new_cell.content = Content.TEMP_TRACK
+            undo = True
         if self.check_constraints():
-            if not cell.right:
-                col += 1
-                if not self.move_to(row, col):
-                    self.undo(cell)
-            if not cell.bottom:
-                row -= 1
-                if not self.move_to(row, col):
-                    self.undo(cell)
-            if not cell.left:
-                col -= 1
-                if not self.move_to(row, col):
-                    self.undo(cell)
-            if not cell.top:
-                row += 1
-                if not self.move_to(row, col):
-                    self.undo(cell)
-
-    def undo(self, cell):
+            if undo:
+                new_cell.content = Content.EMPTY
+            moves = self.moves(new_cell)
+            if from_dir in moves:
+                moves.remove(from_dir)
+            for to_dir in moves:
+                if new_cell.content == Content.EMPTY:
+                    for tr in Track:
+                        if from_dir in tr.name and to_dir in tr.name:
+                            new_cell.track = tr
+                            new_cell.content = Content.TEMP_TRACK
+                            break
+                self.move_from(new_cell, to_dir)
+        # get here if path we took is wrong somewhere
+        if new_cell.content == Content.TEMP_TRACK:
+            new_cell.content = Content.EMPTY
         if cell.content == Content.TEMP_TRACK:
+            cell.draw_track(self.turtle, erase=True)
             cell.content = Content.EMPTY
+
+    def begin(self):
+        new_cell = self.layout[self.start[0]][0]
+        moves = self.moves(new_cell)
+        for to_dir in moves:
+            self.move_from(new_cell, to_dir)
 
 
 board = Layout()
@@ -338,6 +368,6 @@ board.set_start(7)
 board.set_end(5)
 board.set_constraints("2464575286563421")
 board.draw()
-# board.move_to(*board.start, first_move=True)
+board.begin()
 screen = turtle.Screen()
 ans = screen.textinput("Done", "Hit key")
